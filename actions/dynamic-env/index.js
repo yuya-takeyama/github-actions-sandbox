fc5fb255699071ed92b3871e7e3f510a7e961bba
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2022-01-28 19:04:02 +0900

    Test

---
+++ actions/dynamic-env/index.js
@@ -0,0 +1,24 @@
+const appendFileSync = require('fs').appendFileSync;
+const core = require('@actions/core');
+
+const secrets = JSON.parse(core.getInput('secrets', { required: true }));
+const envMaps = {
+  foo: {
+    ENV_1: 'FOO_ENV_1',
+  },
+  bar: {
+    ENV_1: 'BAR_ENV_1',
+    ENV_2: 'BAR_ENV_2',
+  },
+};
+
+const envFile = process.env.GITHUB_ENV;
+const envMap = envMaps[core.getInput('envName', { required: true })];
+
+for (const kv = Object.entries(envMap)) {
+  const envName = kv[0];
+  const secretName = kv[1];
+
+  core.info(`Exporting a secret `${secretName}` as an environment variable ${envName}`);
+  core.exportVariable(envName, secrets[secretName]);
+}
